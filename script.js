c55d7aef50f409d3dc142af4c0d19c3851f5bdd7
gusty <gustavodiorlando@gmail.com>
2023-05-19 10:37:55 -0300

    Add color to squares on mouse hovering

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const container = document.querySelector('.container');
 
-for (let y = 0; y < 16; y++) {
+let gridSize = 16;
+
+for (let x = 0; x < gridSize; x++) {
     const row = document.createElement('div');
     row.classList.add('row');
-    for (let i = 0; i < 16; i++) {
+    for (let y = 0; y < gridSize; y++) {
         const square = document.createElement('div');
         square.classList.add('square');
         row.appendChild(square);
     }
     container.appendChild(row);
 }
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'black';
+    })
+})
