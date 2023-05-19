85986b39d1f4dc8ce5160a6810fef511e77ae340
gusty <gustavodiorlando@gmail.com>
2023-05-19 13:36:34 -0300

    Remove gray scale button + add color mode button

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ for (let x = 0; x < gridSize; x++) {
 let currentMode = 'normal' // Default mode is normal, other modes are rainbow, gray scale etc
 
 const rainbowPicker = document.querySelector('#rainbow-pick');
-const grayScale = document.querySelector('#gray-scale');
 const eraser = document.querySelector('#eraser');
 const colorPicker = document.querySelector('#color-picker');
+const colorMode = document.querySelector('#color-mode')
 
 colorPicker.addEventListener('click', () => {
     currentMode = 'normal';
@@ -29,21 +29,21 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (button.id == 'rainbow-pick') {
-            currentMode = 'rainbow'
-        } else if (button.id == 'gray-scale') {
-            currentMode = 'gray'
+            currentMode = 'rainbow';
         } else if (button.id == 'eraser') {
-            currentMode = 'eraser'
+            currentMode = 'eraser';
         } else if (button.id == 'delete') {
             squares.forEach((square) => {
                 square.style.backgroundColor = 'white';
             })
+        } else if (button.id == 'color-mode') {
+            currentMode = 'normal';
         }
     })
 })
 
 function randomRGB() {
-    return 0 + Math.floor(Math.random() * (255 - 0 + 1));
+    return Math.floor(Math.random() * (255 + 1));
 }
 
 const squares = document.querySelectorAll('.square');
